272528e36453a45fad281e208194e04b8355bf51
Hyunjun Choi <chj7239@gmail.com>
2023-12-28 19:45:04 +0900

    doc: Rest docs 생성

--- src/test/java/com/example/docs/AbstractRestDocsTests.java
+++ src/test/java/com/example/docs/AbstractRestDocsTests.java
@@ -1,12 +1,8 @@
-package com.example.docs.domain.gift;
+package com.example.docs;
 
-import com.example.docs.AbstractRestDocsTests;
-import com.example.docs.RestDocsConfiguration;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
@@ -18,14 +14,26 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(GiftController.class)
-public class GiftControllerTest extends AbstractRestDocsTests {
+@Import(RestDocsConfiguration.class)
+@ExtendWith(RestDocumentationExtension.class)
+public abstract class AbstractRestDocsTests {
 
-    @Test
-    void GiftTest() throws Exception {
-        mockMvc.perform(get("/gift")).andExpect(status().isOk());
+    @Autowired
+    protected RestDocumentationResultHandler restDocs;
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp(
+            final WebApplicationContext context,
+            final RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(restDocumentation))
+                .alwaysDo(MockMvcResultHandlers.print())
+                .alwaysDo(restDocs)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .build();
     }
 }
